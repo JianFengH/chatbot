c077f750b6fcd15ec3ed378bd32e073a8e63835b
thydoge <1301415836@qq.com>
2022-04-20 01:24:53 +0800

    Implementation of a partial search function for climbing trails

---
+++ MaclehoseTrail.py
@@ -0,0 +1,40 @@
+from asyncio import run_coroutine_threadsafe
+import psycopg2
+from config import config
+
+#Define variable "route"
+route = None
+
+def checkroute(routenum):
+    """ Connect to the PostgreSQL database server """
+    
+    #Define global variable "route"
+    global route
+    #get the last number from command "/MACLEHOSE"
+    num = routenum[-1]
+    
+    conn = None
+    try:
+        # read connection parameters
+        params = config('postgresql')
+
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+		
+        # create a cursor and execute the query
+        cur = conn.cursor()
+        cur.execute('select route from maclehose where rid ='+ num)
+        conn.commit()
+        route = cur.fetchall()
+        cur.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return route;
+
+if __name__ == '__main__':
+    checkroute()
--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,49 @@
-from telegram import Update
+from pydoc import text
+from telegram import Update, message
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import logging
 from config import config
+import configparser
+import os
+
+#Use the function from MaclehoseTrail
+from MaclehoseTrail import checkroute
 
 global redis1
 
 def main():
+
+    #////connect the bot
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    updater = Updater(token=(config['telegram']['access_token']),use_context = True)
+    dispatcher = updater.dispatcher
+    
+
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
+    #access_token = config('telegram')['access_token']
+    #logging.info('telegramParams access_token: %s', access_token)
+
+    #////FIVE SUB MACLEHOSE TRAIL
+    dispatcher.add_handler(CommandHandler("MACLEHOSE1",Trail1))
+    dispatcher.add_handler(CommandHandler("MACLEHOSE2",Trail1))
+    dispatcher.add_handler(CommandHandler("MACLEHOSE3",Trail1))
+    dispatcher.add_handler(CommandHandler("MACLEHOSE4",Trail1))
+    dispatcher.add_handler(CommandHandler("MACLEHOSE5",Trail1))
     
-    access_token = config('telegram')['access_token']
 
-    logging.info('telegramParams access_token: %s', access_token)
+    updater.start_polling()
+    updater.idle()
+    
 
+    #////MACLEHOSE TRAIL
+def Trail1(update: Update, context: CallbackContext) -> None:
+    logging.info(str(update.message.text))
+    result =checkroute(update.message.text)
+    update.message.reply_text('here is the route:')
+    context.bot.send_message(chat_id=update.effective_chat.id,text = result)
+    
 
 if __name__ == '__main__':
     main()
